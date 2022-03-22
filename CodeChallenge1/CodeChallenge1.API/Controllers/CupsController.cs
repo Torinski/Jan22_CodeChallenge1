86770d590480502bdb397ab6c87976bd374a5568
Torin J <torinj24@gmail.com>
2022-03-21 21:36:26 -0600

    Added basic Cup Controller

---
+++ CodeChallenge1/CodeChallenge1.API/Controllers/CupsController.cs
@@ -0,0 +1,25 @@
+﻿using CodeChallenge1.Models.ViewModels;
+using CodeChallenge1.Services.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeChallenge1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CupsController : ControllerBase
+    {
+        private readonly ICupService _cupService;
+
+        public CupsController(ICupService cupService)
+        {
+            _cupService = cupService;
+        }
+
+        [HttpPut("swap")]
+        public async Task<ActionResult<CupVM>> SwapCups([FromBody] Array swaps)
+        {
+
+        }
+    }
+}
