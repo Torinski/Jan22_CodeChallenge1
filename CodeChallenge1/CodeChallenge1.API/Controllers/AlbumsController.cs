5fd06bba0ca72719eca34d74552ba92998320ed4
Torin J <torinj24@gmail.com>
2022-03-22 02:04:53 -0600

    Started Album controller

---
+++ CodeChallenge1/CodeChallenge1.API/Controllers/AlbumsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeChallenge1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumsController : ControllerBase
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        [HttpGet]
+        public async Task<HttpResponseMessage> GetAlbums()
+        {
+            var getData = await client.GetAsync("https://jsonplaceholder.typicode.com/albums");
+            return getData;
+        }
+    }
+}
