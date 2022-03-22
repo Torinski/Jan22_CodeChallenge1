a0d589fc96198d0d88354208e2046f05e7873a0e
Torin J <torinj24@gmail.com>
2022-03-22 02:34:10 -0600

    Added album class and finished album endpoint

--- CodeChallenge1/CodeChallenge1.API/Controllers/AlbumsController.cs
+++ CodeChallenge1/CodeChallenge1.API/Controllers/AlbumsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CodeChallenge1.Models.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CodeChallenge1.API.Controllers
 {
@@ -10,10 +12,28 @@ namespace CodeChallenge1.API.Controllers
         static readonly HttpClient client = new HttpClient();
 
         [HttpGet]
-        public async Task<HttpResponseMessage> GetAlbums()
+        public async Task<List<Album>> GetAlbums([FromQuery] string? search = null)
         {
             var getData = await client.GetAsync("https://jsonplaceholder.typicode.com/albums");
-            return getData;
+            var jsonString = await getData.Content.ReadAsStringAsync();
+
+            List<Album> model = JsonConvert.DeserializeObject<List<Album>>(jsonString);
+            if (search == null)
+            {
+                return model;
+            }
+            else
+            {
+                List<Album> searchResults = new List<Album>();
+                foreach (Album album in model)
+                {
+                    if (album.title.ToLower().Contains(search.ToLower()))
+                    {
+                        searchResults.Add(album);
+                    }
+                }
+                return searchResults;
+            }
         }
     }
 }
